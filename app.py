03f38c31c19ad15f95f2132add6c3cec5d26da0f
YasaswiniMallem <165899453+YasaswiniMallem@users.noreply.github.com>
2024-05-31 13:06:29 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+Books = [
+  {
+    'id' : 1,
+    'title' : 'Python - The complete reference',
+    'author' : 'Martin C.Brown',
+    'year' : '2018',
+  },
+{
+  'id' : 2,
+  'title' :'Core Java - An integrated uproach',
+  'author' : 'Dr. R. Nageswara Rao',
+  'year' : '2016',
+},
+{
+  'id' : 3,
+  'title' :' Object oriented programming with C++',
+  'author' :'E Balaguruswamy',
+  'year' :'2020',
+},
+{
+  'id' : 4,
+  'title' : 'SQL- The Ultimate beginners guide ',
+  'author' :'Brandon Cooper',
+  'year' :'2019',
+}
+]
 
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template('home.html', books=Books)
+  
+@app.route("/api/books")
+def list_books():
+  return jsonify(Books)
 
 
 if __name__ == "__main__":
